de7705f68b6345d83456bc8eb90a6178f9761b1a
Danilo P. da Silva <danilopaulinodasilva@gmail.com>
2020-12-02 10:46:58 -0300

    Update vanilla-js-get.js

--- vanilla-js-get.js
+++ vanilla-js-get.js
@@ -1,6 +1,6 @@
-# https://stackoverflow.com/a/979995/6518615
+/* https://stackoverflow.com/a/979995/6518615 */
 
 var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; //window.location.href
 var url = new URL(url_string);
 var c = url.searchParams.get("c");
-console.log(c);
+console.log(c); // m2-m3-m4-m5
